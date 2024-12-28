34e61a8bc1bf480c44572ee98e2bb04264394fb9
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-12-28 18:11:51 +0400

    First commit

---
+++ Day66/Classwork/script.js
@@ -0,0 +1,14 @@
+// let list = []
+
+// for(let i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         list.push(i)
+//     }
+// }
+
+// console.log(list) 
+
+
+for(let i = 0; i<= 10000; i++) {
+    console.log(`${i}, `)
+}
