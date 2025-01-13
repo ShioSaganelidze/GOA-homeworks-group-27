d782f12910785e97093d0c7d410aab3442d0aaf7
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-01-13 17:26:05 +0400

    First commit

---
+++ Day71/Homework/script.js
@@ -0,0 +1,26 @@
+// kata 1:
+
+// function isSortedAndHow(array) {
+//   let ascending = true;
+//   let descending = true;
+  
+//   for (let i = 0; i < array.length - 1; i++) {
+//     if (array[i] < array[i + 1]) {
+//       descending = false;
+//     } else if (array[i] > array[i + 1]) {
+//       ascending = false;
+//     }
+//   }
+  
+//   if (ascending) {
+//     return "yes, ascending";
+//   } else if (descending) {
+//     return "yes, descending";
+//   } else {
+//     return "no";
+//   }
+// }
+
+// kata 2:
+
+//ver gavige
