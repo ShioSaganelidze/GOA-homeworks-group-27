8ace0a15adfb13b6af23ea08c1fe20134766cd7a
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-02-11 20:51:26 +0400

    First commit

---
+++ Day79/Classwork/script.js
@@ -0,0 +1,29 @@
+// function isFlush(cards) {
+//     let counterH = 0;
+//     let counterS = 0;
+//     let counterD = 0;
+//     let counterC = 0;
+//     for (let i = 0; i < cards.length; i++) {
+//       if (cards[i].includes('H')) {
+//         counterH += 1;
+//       }
+      
+//       else if (cards[i].includes('S')) {
+//         counterS += 1;
+//       }
+      
+//       else if (cards[i].includes('D')) {
+//         counterD += 1;
+//       }
+      
+//       else if (cards[i].includes('C')) {
+//         counterC += 1;
+//       }
+//     }
+    
+//     if (counterH === 5 || counterS === 5 || counterD === 5 || counterC === 5) {
+//       return true
+//     } else {
+//       return false
+//     }
+// }
---
+++ Day79/Homework/script.js
@@ -0,0 +1,22 @@
+// function isALeapYear(year) {
+//     this.year = year;
+
+//     if(this.year % 4 == 0 && this.year % 100 != 0 || this.year % 400 == 0) {
+//         return `${this.year} - Yes`
+//     } else {
+//         return `${this.year} - No`
+//     }
+// };
+
+// console.log(isALeapYear(2400));
+
+// function threeDigitSum(num) {
+//     if (num >= 100 && num <= 999) {
+//         let numStr = num.toString();
+//         return Number(numStr[0]) + Number(numStr[1]) + Number(numStr[2]);
+//     } else {
+//         return undefined;
+//     }
+// };
+
+// console.log(threeDigitSum(937));
