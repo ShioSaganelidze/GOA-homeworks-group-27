87040fdd067ef0b5db5cd49a011bb7f1438af7a3
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-02-25 16:23:15 +0400

    First commit

---
+++ Day81/Classwork/script.js
@@ -0,0 +1,21 @@
+// function bonusTime(salary, bonus) {
+//     if (bonus == true) {
+//       return `£${salary*10}`
+//     } else {
+//       return `£${salary}`
+//     }
+// }
+
+// function sumMix(x){  
+//     let sum = 0
+//     for(let i = 0; i<x.length; i++) {
+//       sum += Number(x[i])
+//     }
+//     return sum
+// }
+
+// function minMax(arr){
+//     let min = Math.min(...arr)
+//     let max = Math.max(...arr)
+//     return [min, max]
+// }
