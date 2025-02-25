3f8b29841c40d3476851c3c6997c588af1032f7e
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-02-25 16:26:49 +0400

    First commit

---
+++ Day80/Classwork/script.js
@@ -0,0 +1,21 @@
+// function sum_digits(num) {
+//     let res = 0;
+//     while (num > 0) {
+//         res += num % 10;
+//         num /= 10
+//     }
+//     return Math.round(res)
+// }
+
+// console.log(sum_digits(234))
+
+// function multiTable(number) {
+//     let result = '';
+//     for(let i = 1; i <= 10; i++) {
+//       result += `${i} * ${number} = ${i * number}`;
+//       if (i < 10) {
+//         result += '\n';
+//       }
+//     }
+//     return result;
+// }
---
+++ Day80/Homework/script.js
@@ -0,0 +1,32 @@
+// let names = ['gio', 'nodo', 'nika', 'lasha', 'petre'];
+// names.unshift("gia", "luka");
+// console.log(names);
+
+// let names2 = ['gio', 'nodo', 'nika', 'lasha', 'petre'];
+// names2.shift();
+// console.log(names2);
+
+// let names3 = ['gio', 'nodo', 'nika', 'lasha', 'petre'];
+// names3.splice(1, 2, 'akaki', 'cotne');
+// console.log(names3);
+
+// let names4 = ['gio', 'nodo', 'nika', 'lasha', 'petre'];
+// let res = names4.slice(1, 4);
+// console.log(res);
+
+// let arr1 = ["Cecilie", "Lone"];
+// let arr2 = ["Emil", "Tobias", "Linus"];
+// let children = arr1.concat(arr2);
+// console.log(children);
+
+// let alphabet = ['b', 'c', 'l', 'a'];
+// alphabet.sort();
+// console.log(alphabet);
+
+// let string = [12, 'kia', 'loli', 19];
+// let smth = string.reverse;
+// console.log(smth);
+
+// let arr2 = ['mango', 'banana', 'passion fruit', 'apple'];
+// arr2.join('and');
+// console.log(arr2);
