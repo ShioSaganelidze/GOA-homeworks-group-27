6958bf22ab9fae40150d9f0b8df95204e460696a
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-03-08 15:53:20 +0400

    First commit

---
+++ Day85/Classwork/script.js
@@ -0,0 +1,11 @@
+// function charConcat(string){
+//     let n = string.length;
+//     let middle = Math.floor(n / 2);
+//     let result = [];
+    
+//     for (let i = 0; i < middle; i++) {
+//         result.push(`${string[i]}${string[n - 1 - i]}${i + 1}`);
+//     }
+    
+//     return result.join('');
+// }
---
+++ Day85/Homework/script.js
@@ -0,0 +1,47 @@
+let Id1 = document.getElementById('element1');
+Id1.innerHTML = 'changed1';
+Id1.style.color = 'rgb(255, 0, 0)';
+
+let Id2 = document.getElementById('element2');
+Id2.innerHTML = 'changed2';
+Id2.style.color = 'rgb(255, 127, 0)';
+
+let Id3 = document.getElementById('element3');
+Id3.innerHTML = 'changed3';
+Id3.style.color = 'rgb(255, 255, 0)';
+
+let Id4 = document.getElementById('element4');
+Id4.innerHTML = 'changed4';
+Id4.style.color = 'rgb(0, 255, 0)';
+
+let Class1 = document.getElementsByClassName('class1')[0];
+Class1.innerHTML = 'changedClass1';
+Class1.style.color = 'rgb(0, 0, 255)';
+
+let Class2 = document.getElementsByClassName('class2')[0];
+Class2.innerHTML = 'changedClass2';
+Class2.style.color = 'rgb(75, 0, 130)';
+
+let Class3 = document.getElementsByClassName('class3')[0];
+Class3.innerHTML = 'changedClass3';
+Class3.style.color = 'rgb(148, 0, 211)';
+
+let Class4 = document.getElementsByClassName('class4')[0];
+Class4.innerHTML = 'changedClass4';
+Class4.style.color = 'rgb(255, 105, 180)';
+
+let Button1 = document.getElementsByTagName('button')[0];
+Button1.innerHTML = 'changedTag1';
+Button1.style.color = 'rgb(255, 192, 203)';
+
+let Div2 = document.getElementsByTagName('div')[0];
+Div2.innerHTML = 'changedTag2';
+Div2.style.color = 'rgb(173, 216, 230)';
+
+let H43 = document.getElementsByTagName('h4')[0];
+H43.innerHTML = 'changedTag3';
+H43.style.color = 'rgb(238, 130, 238)';
+
+let H24 = document.getElementsByTagName('h2')[0];
+H24.innerHTML = 'changedTag4';
+H24.style.color = 'rgb(46, 139, 87)';
