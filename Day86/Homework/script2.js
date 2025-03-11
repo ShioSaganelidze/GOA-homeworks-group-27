b5b95b2b119a6bae12abde963e46c449298bea02
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-03-11 19:24:55 +0400

    First commit

---
+++ Day86/Homework/script2.js
@@ -0,0 +1,8 @@
+for(let i = 1; i<11; i++) {
+    let id = document.getElementById(`div${i}`);
+    id.style.backgroundColor = 'blue';
+    id.style.color = 'white';
+    id.style.padding = '10px';
+    id.style.border = '2px solid black';
+    id.style.margin = '10px'
+}
