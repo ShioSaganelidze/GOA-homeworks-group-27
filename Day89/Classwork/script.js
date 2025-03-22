213200e04c3dcbfc47d276eee3f63408e21cddfc
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-03-22 17:48:06 +0400

    First commit

---
+++ Day89/Classwork/script.js
@@ -0,0 +1,40 @@
+// kata 1:
+
+// function countLettersAndDigits(input) {
+//     let lst = [
+//     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+//     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+//     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+//     ];
+//     let sum = 0;
+//     for(let i = 0; i < input.length; i++) {
+//       if (lst.includes(input[i])) {
+//         sum += 1
+//       }
+//     }
+//     return sum
+// }
+
+// kata 2:
+
+// function calculator(a,b,sign){
+//     if (sign == '+') {
+//         return a + b
+//     }
+      
+//     else if (sign === '-') {
+//         return a - b
+//     }
+      
+//     else if (sign === '*') {
+//         return a * b
+//     }
+      
+//     else if (sign === '/') {
+//         return a / b
+//     }
+    
+//     else {
+//       return 'unknown value'
+//     }
+// }
