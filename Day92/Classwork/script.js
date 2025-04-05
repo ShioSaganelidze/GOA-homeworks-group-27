e63ed092ee98948e12d8e436228c1fe0e8c3287a
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-04-05 14:14:50 +0400

    First commit

---
+++ Day92/Classwork/script.js
@@ -0,0 +1,15 @@
+// let num = Number(prompt('Enter a number: '))
+
+// // return num % 2 === 0 ? 'Two' : `${num}`;
+// // return num % 2 === 0 ? `${num} * 8 = ${num * 8}` : num % 2 !== 0 ? `${num} * 9 = ${num * 9}` : null;
+
+// function checkNumber(nums) {
+//     return nums < 0 ? 'Negative' : nums > 0 ? 'Positive' : 'Neither';
+// }
+
+// function randomName() {
+//     let arr = ['sergi', 'andria', 'giorgi rodonaia', 'dachi', 'luka papashvili', 'nodo', 'shio', 'giorgi nozadze', 'nika datashvili', 'zaza', 'irakli', 'kote', 'luka chumbashvili', 'rezi', 'davit wiqrize', 'nika vanishvili', 'luka demetrashivili', 'luka chitidze', 'aleqsandre afxazava', 'nikoloz dvali'];
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
+
+// console.log(randomName());
---
+++ Day92/Homework/script.js
@@ -0,0 +1,22 @@
+// 1:
+
+// let age = Number(prompt('Enter your age: '));
+// return age >= 18 ? 'Adult' : age < 18 && age >= 0 ? 'Child' : NaN;
+
+// 2:
+
+// let num1 = Number(prompt('Enter the first number: '));
+// let num2 = Number(prompt('Enter the second number: '));
+// return num1 < num2 ? 'num2' : num1 > num2 ? 'num1' : num1 - num2 == 0 ? 'Equal' : NaN;
+
+// 3:
+
+// function fakeBin(x) {
+//     let res = '';
+  
+//     for(let i = 0; i < x.length; i++) {
+//       x[i] < 5 ? res += '0' : res += '1';
+//     }
+  
+//     return res;
+// }
