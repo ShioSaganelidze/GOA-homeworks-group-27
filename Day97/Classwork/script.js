d956dadc8dcdebd80a95d0e34dc014c0b46028da
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-04-26 18:03:58 +0400

    First commit

---
+++ Day97/Classwork/script.js
@@ -0,0 +1,29 @@
+// kata 1:
+
+// ver gavakete
+
+// kata 2:
+
+// function whoIsPaying(name) {
+//     let res2 = ''
+//     let res = []
+//     for (let i = 0; i < name.length; i++) {
+//         res2 += name[i]
+//         if (i === 1) {
+//             res.push(res2)
+//             break
+//         }
+//     }
+//     res.push(name)
+//     return name.length <= 2 ? [name] : res.reverse()
+// }
+
+// kata 3:
+
+// function nextId(ids){
+// 	let i = 0;
+// 	while (ids.includes(i)) {
+// 		i++
+// 	}
+// 	return i;
+// }
