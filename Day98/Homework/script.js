1d62c033ea3f9a62e4894f66771a3e00c826c21c
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-04-28 18:07:30 +0400

    First commit

---
+++ Day98/Homework/script.js
@@ -0,0 +1,36 @@
+// kata 1:
+
+// ver gavige
+
+// kata 2:
+
+// ver gavige
+
+// kata 3:
+
+// function nthChar(words) {
+//     let n = 0;
+//     let res = '';
+//     for (let i = 0; i < words.length; i++) {
+//       res += words[i][n];
+//       n++;
+//     }
+//     return res;
+// }
+
+// kata 4:
+
+// function missingNo(nums) {
+//     let Esum = 5050;
+//     let Rsum = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//       Rsum += nums[i];
+//     }
+//     return Esum - Rsum;
+// }
+
+// kata 5:
+
+// function betweenExtremes(numbers) {
+//     return Math.abs(Math.min(...numbers) - Math.max(...numbers));
+// }
