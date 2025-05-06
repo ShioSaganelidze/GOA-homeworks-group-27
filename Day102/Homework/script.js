5bde42a7564addfa17f40947da2071f286071630
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2025-05-06 18:01:34 +0400

    First commit

---
+++ Day102/Homework/script.js
@@ -0,0 +1,111 @@
+// Task 1:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Div has been clicked');
+// });
+
+// document.getElementById('btn1').addEventListener('click', () => {
+//     console.log('Button has been clicked');
+// });
+
+// Task 2:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Div1 has been clicked');
+// });
+
+// document.getElementById('div2').addEventListener('click', () => {
+//     console.log('Div2 has been clicked');
+// });
+
+// document.getElementById('div3').addEventListener('click', () => {
+//     console.log('Div3 has been clicked');
+// });
+
+// Task 3:
+
+// document.getElementsByTagName('body')[0].addEventListener('click', () => {
+//     console.log('Body was clicked');
+// });
+
+// document.getElementById('btn1').addEventListener('click', () => {
+//     console.log('Button has been clicked');
+// });
+
+// Task 4:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Parent clicked');
+// });
+
+// document.getElementById('div2').addEventListener('click', () => {
+//     console.log('Child clicked');
+// });
+
+// Task 5:
+
+// document.getElementById('btn1').addEventListener('click', () => {
+//     console.log('Grandparent has been clicked');
+// });
+
+// document.getElementById('btn2').addEventListener('click', () => {
+//     console.log('Parent has been clicked');
+// });
+
+// document.getElementById('btn3').addEventListener('click', () => {
+//     console.log('Child has been clicked');
+// });
+
+// Task 6:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Parent has been clicked');
+// }, true);
+
+// document.getElementById('div2').addEventListener('click', () => {
+//     console.log('Child has been clicked');
+// }, true);
+
+// Task 7:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Parent clicked');
+// }, true);
+
+// document.getElementById('div2').addEventListener('click', () => {
+//     console.log('Child clicked');
+// });
+
+// Task 8:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Grandparent has been clicked');
+// }, true);
+
+// document.getElementById('div2').addEventListener('click', () => {
+//     console.log('Parent has been clicked');
+// }, true);
+
+// document.getElementById('div3').addEventListener('click', () => {
+//     console.log('Child has been clicked');
+// }, true);
+
+// Task 9:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Capture has been clicked');
+// }, true);
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Bubble has been clicked');
+// });
+
+// Task 10:
+
+// document.getElementById('div1').addEventListener('click', () => {
+//     console.log('Capture has been clicked');
+// }, true);
+
+// document.getElementById('btn1').addEventListener('click', () => {
+//     console.log('Bubble has been clicked');
+// });
